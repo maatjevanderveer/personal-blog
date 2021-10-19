f78e037d1500244a40c62cccfde0b91254007a39
Maatje van der Veer <mjvanderveer@gmail.com>
2021-10-19 17:02:56 +0200

    set up layout

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Layout from '../../components/layout';
+import Head from 'next/head';
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>My personal blog - How to set up a project with Next.js (React), Storybook and Figma 3</title>
+            </Head>
+            <h1>How to set up a project with Next.js (React), Storybook and Figma 2</h1>
+            <p>Lorem ipsum</p>
+        </Layout>
+    )
+}
